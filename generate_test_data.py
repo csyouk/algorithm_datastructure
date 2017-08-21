4ff3fca802d5d3633fc74577cc34b4be343baed1
Youk Chang SU <csyouk@gmail.com>
2017-08-21 21:08:01 +0900

    make test data set script

--- generate_test_data.py
+++ generate_test_data.py
@@ -1,1 +1,12 @@
+import sys
 from random import randint
+
+DATA_LEN = 20000
+
+f = open("test_data.txt", 'w')
+sys.stdout = f
+# print(DATA_LEN)
+for _ in range(1, DATA_LEN + 1):
+    print(randint(-DATA_LEN, DATA_LEN), end = " ")
+
+f.close()
