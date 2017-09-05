3b511288e3e431117a9a8c57c77a2b3ed1dda08a
Youk Chang SU <csyouk@gmail.com>
2017-09-05 21:05:39 +0900

    update

--- algo_data/DFS.c
+++ algo_data/DFS.c
@@ -976,10 +976,191 @@ int main(void){
 #endif
 
 
-// 다시 도전하는 N Queen
+// 다시 도전하는 N Queen + 속도 향상
+#if 0
+#include <stdio.h>
+#define SIZE 20
+#define N_PROMISING 0
+#define PROMISING 1
+#define PUT_QUEEN 1
+#define COLLECT_QUEEN 0
+#define QUEEN_ALREADY_EIXT 1
+
+#define VISITED 1
+#define N_VISITED 0
+
+int chess[SIZE][SIZE];
+int col_visited[SIZE];
+int N, cnt;
+
+int promising(int r, int c, int N){
+	int i, j;
+
+	// | 방향
+	for (i = r, j = c; i > 0; i--){
+		if (chess[i][c]) return N_PROMISING;
+	}
+
+	// \ 방향
+	for (i = r, j = c; (i > 0 && j > 0); i--, j--)
+	{
+		if (chess[i][j]) return N_PROMISING;
+	}
+
+	// / 방향
+	for (i = r, j = c; (i > 0 && j < N+1) ; i--, j++)
+	{
+		if (chess[i][j]) return N_PROMISING;
+	}
+
+	return PROMISING;
+}
+
+void DFS(int n, int N){
+	int i;
+	if (n > N) { cnt++; return; }
+
+	for (i = 1; i <= N; i++){
+		if (col_visited[i]) continue;
+		if (promising(n, i, N) == N_PROMISING) continue;
+		//chess[n][i] = PUT_QUEEN;
+		col_visited[i] = VISITED;
+		DFS(n + 1, N);
+		col_visited[i] = N_VISITED;
+		//chess[n][i] = COLLECT_QUEEN;
+	}
+}
+
+int main(void){
+	scanf("%d", &N);
+
+	DFS(1, N);
+
+	printf("%d", cnt);
+
+	return 0;
+}
+#endif
+
+// N Queen 속도 향상 버전
+#if 0
+
+#pragma warning (disable:4996)
+#include <stdio.h>
+#define ABS(a) ((a>0)?(a):(-(a)))
+
+int N, CNT;
+int b[10 + 10]; // 저장배열 : b[row] ? 해당 row에 위치한 퀸의 col값 저장. 
+int Y[10 + 10]; // 중복방지 체크배열 : Y[col] = 1 ? 해당 col값은 이미 퀸이 존재해서 불가함.
+
+int check(int row, int col){ // 대각선 체크 함수.
+	int i;
+	for (i = 1; i < row; i++)
+	{
+		if ((row - i) == ABS(b[i] - col)) return 1;
+		// 가로차이와 세로차이가 같다면 대각선상으로 인식하고 TRUE를 리턴한다.
+	}
+	return 0;
+}
+
+void DFS(int n) {
+
+	// leaf. (종료 조건)
+	if (n > N)
+	{
+		CNT++; // 끝까지 return 되지 않고 도달 가능하다면, 갯수를 센다.
+		return;
+	}
+
+	// multi tree. : 이곳에서 각 행(n)에 대해서 열(j)를 선택한다.
+	int j;
+	for (j = 1; j <= N; j++)
+	{
+		// Y[j]를 통한 <열 체크> 그리고 check(n,j)를 통한 <대각선 체크>
+		// NOTICE : <행 체크>는 필요가 없다.
+		if (Y[j]) continue;
+		if (check(n, j)) continue;
+		Y[j] = 1;
+		{
+			b[n] = j;
+			DFS(n + 1);
+		}
+		Y[j] = 0;
+	}
+	return;
+}
+
+int main(void) {
+	scanf("%d", &N);
+	DFS(1);
+	printf("%d", CNT);
+	return 0;
+}
+#endif
+
+
+// 문제 번호 : [TST]보너스
 #if 1
+#include <stdio.h>
+#define SIZE 20
+#define CONN_SIZE 110
+#define SUB 0
+#define N_SUB 1
+#define DONE 1
+
+typedef struct {
+	int s, e;
+} Conn;
+
+Conn cases[CONN_SIZE];
+int bonus[SIZE], people[SIZE], result[SIZE];
+int N, M, flag;
+
+
+int is_i_sub(int n, int i){
+	int j;
+
+	for ( j = 1; j <= M; j++)
+	{
+		if (people[j]) continue;
+		if (cases[j].s == n && cases[j].e == i) return SUB;
+	}
+	return N_SUB;
+}
+
+void DFS(int n){
+	int i;
+	if (flag) return;
+	if (n > N) { flag = 1; return; }
+
+	//for (i = 2; i <=M; i++)
+	//{
+	//	if (is_i_sub(n, i) == N_SUB) continue;
+	//	result[i] = bonus[n+1];
+	//	people[i] = DONE;
+	//	DFS(n + 1);
+	//}
+	for (i = 1; i <= M; i++)
+	{
+
+		DFS
+	}
+}
+
+
 int main(void){
 
+	freopen("in.txt", "r", stdin);
+
+	int i;
+	scanf("%d %d", &N, &M);
+
+	for (i = 1; i <= M; i++) scanf("%d %d", &cases[i].s, &cases[i].e);
+	for (i = 1; i <= N; i++) scanf("%d ", &bonus[i]);
+
+	DFS(1);
+
+	for (i = 1; i <= N; i++) printf("%d ", result[i]);
 
 
 	return 0;
