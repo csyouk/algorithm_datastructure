e7450f4cd580427dadb20d481cdaa81262a1e874
Youk Chang SU <csyouk@gmail.com>
2017-09-19 21:54:06 +0900

    update

--- algo_data/구슬집어넣기게임.c
+++ algo_data/구슬집어넣기게임.c
@@ -1,115 +1,137 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-#define SIZE 20+10
+#define SIZE 30
 #define DIRS 4
+
 typedef struct {
-	int ri, rj, rdis;
-	int bi, bj, bdis;
+	int ri, rj;
+	int bi, bj;
+	int cnt, dis;
 } Bead;
 
 typedef struct {
 	int i, j;
 } Hall;
 
-Bead Q[SIZE * SIZE];
 Bead bead;
 Hall hall;
-int T, R, C,RP,WP;
 
-char raw[SIZE][SIZE];
+Bead Q[10000];
+
+int rp, wp;
+int T, R, C;
+char Raw[SIZE][SIZE];
 int map[SIZE][SIZE];
 int visited[SIZE][SIZE];
-int di[DIRS] = {0,-1,0,1};
-int dj[DIRS] = {-1,0,1,0};
+
+int di[DIRS] = { 0, -1, 0, 1 };
+int dj[DIRS] = { -1, 0, 1, 0 };
 
 void init(){
 	int i, j;
-	for ( i = 0; i <SIZE; i++)
+	for ( i = 0; i < SIZE; i++)
 	{
-		for ( j = 0; j <SIZE; j++)
+		for ( j = 0; j < SIZE; j++)
 		{
-			visited[i][j] = 0;
+			map[i][j] = visited[i][j] = 0;
 		}
 	}
+	rp = wp = 0;
 }
-int replace(char sym,int i, int j){
+int re(char sym, int i, int j){
 	switch (sym)
 	{
 	case '#': return 1;
 	case '.': return 0;
-	case 'H': (hall.i = i, hall.j = j); return -1;
-	case 'B': (bead.bi = i, bead.bj = j); return 2;
-	case 'R': (bead.ri = i, bead.rj = j); return 3;
-	default: break;
+	case 'R': 
+		bead.ri = i; bead.rj = j;
+		visited[i][j] = 1;
+		return 0;
+	case 'B': 
+		bead.bi = i; bead.bj = j;
+		return 0;
+	case 'H':
+		hall.i = i; hall.j = j;
+		return 0;
+	default:
+		break;
 	}
+	return - 10;
 }
-
-void convert(){
+void gen(void){
 	int i, j;
-	for (i = 1; i <=R; i++)
-	{
-		for (j = 1; j <=C; j++)
-		{
-			map[i][j] = replace(raw[i][j], i, j);
+	for ( i = 1; i <= R; i++){
+		for ( j = 1; j <= C; j++){
+			map[i][j] = re(Raw[i][j], i, j);
 		}
 	}
 }
 
+int BFS(void){
+	int k, nri,nrj, nbi,nbj;
+	Bead out;
 
+	Q[wp++] = bead;
 
-int BFS(){
-	int k, nri, nrj, nbi, nbj;
-	Bead out, in;
-	Q[WP++] = bead;
-	visited[bead.ri][bead.rj] = 1;
-
-	while (RP < WP)
+	while (rp < wp)
 	{
-		out = Q[RP++];
-		if (out.rdis > 10) continue;
-		if (out.ri == hall.i && out.rj == hall.j) return out.rdis;
+		out = Q[rp++];
+
+		// 10회 이상 기울였으면 실패
+		if (out.cnt > 10) return -1;
+
+		// 빨강공이 구멍에 들어갔으면 성공. 이동거리 리턴
+		if (out.ri == hall.i && out.rj == hall.j) return out.dis;
+
+		// 4방향으로 기울여본다.
 		for (k = 0; k < DIRS; k++)
 		{
-			nri = out.ri + di[k]; nrj = out.rj + dj[k];
-			
-			//nbi = out.bi + di[k]; nbj = out.bj + dj[k];
-			// 파랑공 nbi,nbj 위치 계산하기.
-			// 벽을 만나는 경우 위치를 움직이지 않는다.
-			//if (map[nbi][nbj]) { nbi = out.bi; nbj = out.bj; }
-			if (nri < 1 || nri > R || nrj < 1 || nrj > C) continue;
+			nri = out.ri + di[k];   nrj = out.rj + dj[k];
+			nbi = out.bi + di[k];   nbj = out.bj + dj[k];
+
+			// 파랑공이 홀로 빠지면 게임 종료이므로 건너뜀.	
+			if (nbi == hall.i && nbj == hall.j) continue;
+
+			// 이미 방문했던 곳은 건너뜀.
 			if (visited[nri][nrj]) continue;
-			if (map[nri][nrj]) continue;
-			//두 공이 만나는 경우.
-			//if (nri == nbi || nrj == nbj) continue;
-			// 파랑공이 홀로 빠지는 경우.
-			//if (nbi == hall.i && nbj == hall.j) continue;
-			// 파랑공이 범위를 벗어나는 경우.
-			//if (nbi < 1 || nbi > R || nbj < 1 || nbj > C) continue;
+
+			// 벽으로 막혀있으면, 움직이지 않는다. 
+			if (map[nri][nrj]){ nri = out.ri; nrj = out.rj; }
+			if (map[nbi][nbj]){ nri = out.bi; nrj = out.bj; }
+
+
+			// 빨강, 파랑 공이 만나게 되는 경우는 종료되므로, 건너뛴다.
+			if (nri == nbi && nrj == nbj) continue;
 
 			visited[nri][nrj] = 1;
-			in.ri = nri;  in.rj = nrj;  in.rdis = out.rdis + 1;
-			//in.bi = nbi;  in.bj = nbj;
-			Q[WP++] = in;
+
+			Q[wp].ri = nri;  Q[wp].rj = nrj;
+			Q[wp].bi = nbi;  Q[wp].bj = nbj;
+			Q[wp].cnt = out.cnt + 1;
+			Q[wp].dis = out.dis + 1;
+
+			wp++;
 		}
 	}
-	// 불가능한 경우.
+
 	return -1;
 }
-int main(void){
-	int i, j;
-	scanf("%d", &T);
 
-	for (i = 1; i <=T; i++)
+int main(void)
+{
+	int i;
+
+	scanf("%d", &T);
+	while (T--)
 	{
 		init();
 		scanf("%d %d", &R, &C);
-		for ( j = 1; j <=R; j++){
-			scanf("%s", &raw[j][1]);
+		for ( i = 1; i <= R; i++)
+		{
+			scanf("%s", &Raw[i][1]);
 		}
-		convert();
+		gen();
 		printf("%d\n", BFS());
 	}
-
 	return 0;
 }
