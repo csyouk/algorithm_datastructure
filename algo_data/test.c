2a8b4761582de33dfc43dfb089dbdc661c98c4e2
Youk Chang SU <csyouk@gmail.com>
2017-10-10 17:56:18 +0900

    update

--- algo_data/test.c
+++
@@ -1,9 +0,0 @@
-//int main(void){
-//
-//	signed int sx = 0x80000001;
-//	unsigned int ux = 0x80000001;
-//
-//	printf("%X, %X \n", sx >> 2, ux >> 2);
-//	printf("%X, %X \n", sx << 2, ux << 2);
-//	return 0;
-//}
